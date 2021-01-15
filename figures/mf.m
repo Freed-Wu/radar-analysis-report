ab1e138d4834dfbdce70e196e61a08d9a0d8aa1b
Freed-Wu <Wuzy01@qq.com>
2021-01-15 21:28:53 +0800

    Fix mf.tex

--- figures/mf.m
+++ figures/mf.m
@@ -3,7 +3,7 @@ s_mrect = [1 0 1 0 1];
 L = 5;
 % m_sequence comes from <https://github.com/askuric/m_sequences_tools>
 s_pn = m_sequence(2, L);
-% if you don't wanna download the aforementioned packages, uncomment it
+% uncomment it if you don't have the aforementioned package
 % s_pn = [0 1 1 1 0 0 0 1 0 1 0 1 1 0 1 0 0 0 0 1 1 0 0 1 0 0 1 1 1 1 1];
 % the period number of m sequence
 N = 10;
@@ -20,7 +20,6 @@ s_barker7 = [1 1 1 -1 -1 1 -1];
 s_barker4times7 = s_barker7'*s_barker4;
 s_barker4_7 = s_barker4times7(:);
 for k = {'rect' 'mrect' 'm' 'pn' 'barker13' 'barker4_7'}
-for k = {'m'}
 	name = k{:};
 	eval(['s = s_' name ';']);
 	stem(s);
